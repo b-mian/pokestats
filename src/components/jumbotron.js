5d1e034d2cc37709e018e48f4aa64252347553d8
b-mian <42081057+b-mian@users.noreply.github.com>
2022-07-10 16:16:30 -0400

    Tweaking CSS and other Styles
    Improving UI Design of site...

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -6,10 +6,10 @@ const Jumbotron = () => {
                 <div className="row justify-content-center">
                     <h1 className="col-xs-12">PokeStats</h1>
                 </div>
+                <hr className="col-xs-12"></hr>
                 <div className="row justify-content-center">
                     <h3 className="col-xs-12">Pokemon by numbers</h3>
                 </div>
-                <hr className="col-xs-12"></hr>
             </div>
         </div>
     );
